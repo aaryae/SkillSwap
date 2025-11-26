b7d9fb7571782cb1e740d39021c3d33897c3af0b
aaryae <aaryaed@gmail.com>
2025-11-27 04:12:08 +0545

    feat:request controller added

---
+++ request-service/src/main/java/org/example/skillswap/controller/RequestController.java
@@ -0,0 +1,51 @@
+package org.example.skillswap.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.example.skillswap.dto.request.CreateRequest;
+import org.example.skillswap.dto.request.UpdateRequest;
+import org.example.skillswap.dto.response.RequestResponse;
+import org.example.skillswap.service.RequestService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/requests")
+@RequiredArgsConstructor
+public class RequestController {
+
+    private final RequestService requestService;
+
+    @PostMapping
+    public ResponseEntity<String> createRequest(
+            @RequestBody CreateRequest request,
+            HttpServletRequest httpRequest) {
+
+        String requester = httpRequest.getHeader("X-User-Email");
+        requestService.createRequest(requester, request);
+        return ResponseEntity.ok("Request sent successfully.");
+    }
+
+    @GetMapping("/sent")
+    public ResponseEntity<List<RequestResponse>> sentRequests(HttpServletRequest req) {
+        String email = req.getHeader("X-User-Email");
+        return ResponseEntity.ok(requestService.getMyRequests(email));
+    }
+
+    @GetMapping("/received")
+    public ResponseEntity<List<RequestResponse>> receivedRequests(HttpServletRequest req) {
+        String email = req.getHeader("X-User-Email");
+        return ResponseEntity.ok(requestService.getRequestsForMe(email));
+    }
+
+    @PostMapping("/{id}/status")
+    public ResponseEntity<String> updateStatus(
+            @PathVariable UUID id,
+            @RequestBody UpdateRequest request) {
+        requestService.updateStatus(id, String.valueOf(request.status()));
+        return ResponseEntity.ok("Status updated");
+    }
+}
